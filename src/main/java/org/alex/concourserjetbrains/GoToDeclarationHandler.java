7067b2fc1dae035713ca933c34d6f2bcee7ff2a1
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2026-03-29 16:52:49 +0300

    Handle ctrl+click for all cases

--- src/main/java/org/alex/concourserjetbrains/GoToDeclarationHandler.java
+++ src/main/java/org/alex/concourserjetbrains/GoToDeclarationHandler.java
@@ -2,20 +2,22 @@ package org.alex.concourserjetbrains;
 
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
-import groovy.util.logging.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
@@ -23,6 +25,10 @@ import static java.lang.String.format;
 public class GoToDeclarationHandler implements GotoDeclarationHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GoToDeclarationHandler.class);
+    private static final String INLINE_PYTHON_RUN = "python3";
+    private static final List<String> PYTHON_RESOLVABLE_PARAMS = List.of("SCRIPT_PATH", "run", "py", INLINE_PYTHON_RUN);
+    private static final List<String> YAML_RESOLVABLE_PARAMS = List.of("file", "PATH", "FILE");
+
 
     @Override
     public PsiElement @Nullable [] getGotoDeclarationTargets(@Nullable PsiElement sourceElement, int offset, Editor editor) {
@@ -33,10 +39,18 @@ public class GoToDeclarationHandler implements GotoDeclarationHandler {
             return null;
         }
         Configuration configuration = ConfigurationService.get(editor.getProject()).getConfig();
+        String clickedLine = getClickedLine(editor.getDocument(), offset);
+        if (!isClickedLineResolvable(clickedLine)) {
+            return null;
+        }
 
         if (sourceElement == null) return null;
-        var text = sourceElement.getText();
-        String fullPath = resolveFilePath(text, configuration);
+
+        var clickedContent = sourceElement.getText();
+        System.out.println("ALEX: Clicked content: "+clickedContent);
+
+        String fullPath = resolvePath(clickedLine, clickedContent, configuration);
+        System.out.println("ALEX: Full path: "+fullPath);
         VirtualFile virtualFile = LocalFileSystem.getInstance()
                 .findFileByPath(fullPath);
 
@@ -47,6 +61,51 @@ public class GoToDeclarationHandler implements GotoDeclarationHandler {
             }
         });
         return null;
+    }
+
+    private String getClickedLine(Document document, int offset) {
+        int lineNumber = document.getLineNumber(offset);
+        int lineStart = document.getLineStartOffset(lineNumber);
+        int lineEnd = document.getLineEndOffset(lineNumber);
+        return document.getText(new TextRange(lineStart, lineEnd));
+    }
+
+    private boolean isClickedLineResolvable(String clickedLine) {
+        List<String> allParams = new ArrayList<>();
+        allParams.addAll(YAML_RESOLVABLE_PARAMS);
+        allParams.addAll(PYTHON_RESOLVABLE_PARAMS);
+        for (String param : allParams) {
+            if (clickedLine.contains(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String resolvePath(String clickedLine, String relativePath, Configuration configuration) {
+        if (isPythonPath(clickedLine)) {
+            return resolvePythonPath(relativePath, configuration);
+        }
+        return resolveFilePath(relativePath, configuration);
+    }
+
+    private boolean isPythonPath(String path) {
+        for (String param: PYTHON_RESOLVABLE_PARAMS) {
+            if (path.contains(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String resolvePythonPath(String relativePath, Configuration configuration) {
+        if (relativePath.startsWith(INLINE_PYTHON_RUN)) {
+            relativePath = relativePath.replaceFirst(format("%s -m", INLINE_PYTHON_RUN), "");
+        }
+        String normalisedPath = relativePath.replace(".", "/");
+        normalisedPath = normalisedPath.trim();
+        normalisedPath += ".py";
+        return resolveFilePath(normalisedPath, configuration);
     }
 
     private String resolveFilePath(String relativePath, Configuration configuration) {
